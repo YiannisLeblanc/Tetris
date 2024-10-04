5f03eb1df7678db9978f0ac4dd8054543de0faa0
Yiannis <yiannis.leblanc@icloud.com>
2024-10-04 18:26:21 +0200

    update

--- tetris_game.h
+++ tetris_game.h
@@ -1,6 +1,7 @@
 #ifndef TETRIS_GAME_H
 #define TETRIS_GAME_H
 
-#include "Screen.h"
+#include "Grid.h"
+
 
 #endif
---
+++ tetris_input.h
@@ -0,0 +1,9 @@
+#ifndef TETRIS_INPUT_H
+#define TETRIS_INPUT_H
+
+bool rotateR();
+bool rotateR_hold();
+bool rotateL();
+bool rotateL_hold();
+
+#endif	
